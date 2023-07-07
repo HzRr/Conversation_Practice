b54a713975832681b2f115263371486a3e6e0925
hzrr <origami_suki@outlook.com>
2023-07-07 19:16:28 +0800

    realize some function

---
+++ Home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+def home():
+    # Set title
+    st.title("Conversation Practice")
+
+    # Show detail information about the application
+    st.markdown("Welcome to the Conversation Practice app!")
+
+if __name__ == "__main__":  
+    home()
---
+++ pages/3-Settings.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+def settings():
+    # TODO
+    pass
+
+settings()
